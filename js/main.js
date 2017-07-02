d375385fa32186b712cfdbb7e4a2b3ad34f3a397
Maciej Abramowski <m.abramowski@wp.pl>
2017-07-02 13:35:39 +0200

    portfolio1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
 'use strict';
 $(function(){
     
-$(window).scroll(function(){
-    
-if ($(document).scrollTop() > 50){
-document.getElementById('nav-grab').classList.add('blue-background');
-} 
-if ($(document).scrollTop() < 50){
-   document.getElementById('nav-grab').classList.remove('blue-background'); 
-}
+//if ($(document).scrollTop() > 50){
+////document.getElementById('nav-grab').classList.add('blue-background');
+//$('#nav-grab').addClass('blue-background');
+//} 
 
-    });
+scrollNav();
+    
+$(window).scroll(function(){
+    scrollNav();
+});
+    
+//if ($(document).scrollTop() > 50){
+////document.getElementById('nav-grab').classList.add('blue-background');
+//$('#nav-grab').addClass('blue-background');
+//} 
+//else {
+////   document.getElementById('nav-grab').classList.remove('blue-background'); 
+//    $('#nav-grab').removeClass('blue-background');
+//}
+
+    
+    
+function scrollNav(){
+    if ($(window).scrollTop() > 50){
+//document.getElementById('nav-grab').classList.add('blue-background');
+$('#nav-grab').addClass('blue-background');
+} 
+else {
+//   document.getElementById('nav-grab').classList.remove('blue-background'); 
+    $('#nav-grab').removeClass('blue-background');
+}
+}
     
 });
